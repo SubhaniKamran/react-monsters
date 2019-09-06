04b944d558d939ff65a68ec6b306fd32b713328e
SubhaniKamran <itskamransubhani@gmail.com>
2019-09-06 11:40:05 -0700

    Updates

---
+++ precache-manifest.6556f5eea35528390b1acc43ae3fcc15.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd8cebfc82119996a599389043fc47d6",
+    "url": "/react-monsters/index.html"
+  },
+  {
+    "revision": "e39c565ee3021e144f2e",
+    "url": "/react-monsters/static/css/main.d42ca80e.chunk.css"
+  },
+  {
+    "revision": "f7abcd3b9d99ae6d26ef",
+    "url": "/react-monsters/static/js/2.bffc00c9.chunk.js"
+  },
+  {
+    "revision": "e39c565ee3021e144f2e",
+    "url": "/react-monsters/static/js/main.1bf409dd.chunk.js"
+  },
+  {
+    "revision": "0248dbfb501e164e0dfc",
+    "url": "/react-monsters/static/js/runtime~main.9b2e0b25.js"
+  }
+]);
